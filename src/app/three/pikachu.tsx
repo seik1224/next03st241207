f0480f5cadcde406941757b61fc26cf10fe62f16
seik <seik1224@naver.com>
2024-12-07 16:27:06 +0900

    .

--- src/app/three/pikachu.tsx
+++ src/app/three/pikachu.tsx
@@ -4,8 +4,8 @@ Command: npx gltfjsx@6.5.3 public/Pikachu.glb -o src/app/three/pikachu.tsx
 */
 
 import * as THREE from 'three'
-import React from 'react'
-import { useGraph } from '@react-three/fiber'
+import React, { useEffect, useMemo } from 'react'
+import { useFrame, useGraph } from '@react-three/fiber'
 import { useGLTF, useAnimations } from '@react-three/drei'
 import { GLTF, SkeletonUtils } from 'three-stdlib'
 
@@ -36,6 +36,30 @@ export function Model(props: JSX.IntrinsicElements['group']) {
   const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene])
   const { nodes, materials } = useGraph(clone) as GLTFResult
   const { actions } = useAnimations(animations, group)
+
+  useEffect(()=>{
+    actions['Happy']?.fadeIn(0.5).play();
+    return()=>{
+      actions['Happy']?.fadeOut(0.5).stop();
+    };
+  },[actions]);
+
+  useFrame(()=>{
+    if(group.current){
+      group.current.rotation.y += 0.1;
+    }
+  })
+
+  const greenMaterial = useMemo(
+    ()=> (
+      new THREE.MeshStandardMaterial({
+        color: '#00ff00',
+        metalness : 0.7,
+        roughness : 0.2
+      })
+    ),
+    []
+  )
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
@@ -44,7 +68,9 @@ export function Model(props: JSX.IntrinsicElements['group']) {
           <group name="pikachu">
             <skinnedMesh name="pikachu_1" geometry={nodes.pikachu_1.geometry} material={materials.mouth} skeleton={nodes.pikachu_1.skeleton} />
             <skinnedMesh name="pikachu_2" geometry={nodes.pikachu_2.geometry} material={materials.eye} skeleton={nodes.pikachu_2.skeleton} />
-            <skinnedMesh name="pikachu_3" geometry={nodes.pikachu_3.geometry} material={materials.body} skeleton={nodes.pikachu_3.skeleton} />
+            <skinnedMesh name="pikachu_3" geometry={nodes.pikachu_3.geometry} material={greenMaterial} skeleton={nodes.pikachu_3.skeleton}>
+              <meshStandardMaterial color='blue' />
+            </skinnedMesh>
           </group>
         </group>
       </group>
